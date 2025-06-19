7fc5d9c9eab1e68983e1e4dd90aa4717d7e5363b
samitkumarpatel <samitkumarpatel@gmail.com>
2025-06-19 23:54:39 +0200

    Enhancement

--- src/main/java/net/samitkumar/user_management/UserManagementApplication.java
+++ src/main/java/net/samitkumar/user_management/UserManagementApplication.java
@@ -1,16 +1,17 @@
 package net.samitkumar.user_management;
 
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -23,8 +24,6 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -61,9 +60,20 @@ class RouterHandler {
 	final UserRepository userRepository;
 
 	public Mono<ServerResponse> allUser(ServerRequest request) {
-		return Flux.merge(jsonPlaceholderClient.getAllUser(),userRepository.findAll())
+		return
+				jsonPlaceholderClient.getAllUser()
+						.collectList()
+						.zipWith(userRepository.findAll().collectList(), (u1, u2) -> {
+							log.info("Fetched {} external users and {} internal users", u1.size(), u2.size());
+							var externalUsers = u1.stream().map(user -> new User(user.id(), user.name(), user.username(), user.email(), user.address(), UserType.EXTERNAL));
+							var internalUsers = u2.stream().map(user -> new User(user.id(), user.name(), user.username(), user.email(), user.address(), UserType.INTERNAL));
+							return Stream.concat(externalUsers, internalUsers).toList();
+						})
+						.flatMap(ServerResponse.ok()::bodyValue);
+
+		/*return Flux.merge(jsonPlaceholderClient.getAllUser(), userRepository.findAll())
 				.collectList()
-				.flatMap(ServerResponse.ok()::bodyValue);
+				.flatMap(ServerResponse.ok()::bodyValue);*/
 	}
 
 	public Mono<ServerResponse> createNewUser(ServerRequest request) {
@@ -77,11 +87,13 @@ class RouterHandler {
 		var userId = request.pathVariable("id");
 		log.info("Fetching user with ID: {}", userId);
 		Mono<User> externalUser = jsonPlaceholderClient.getUserById(userId)
+				.map(user -> new User(user.id(), user.name(), user.username(), user.email(), user.address(), UserType.EXTERNAL))
 				.doOnNext(user -> log.info("jsonPlaceholderClient User: {}", user))
 				.onErrorResume(e -> Mono.empty())
 				.switchIfEmpty(Mono.empty());
 
 		Mono<User> dbUser = userRepository.findById(userId)
+				.map(user -> new User(user.id(), user.name(), user.username(), user.email(), user.address(), UserType.INTERNAL))
 				.doOnNext(user -> log.info("userRepository User: {}", user))
 				.switchIfEmpty(Mono.empty());
 
@@ -91,9 +103,10 @@ class RouterHandler {
 				.switchIfEmpty(ServerResponse.notFound().build());
 	}
 }
+enum UserType { EXTERNAL, INTERNAL;}
 
 @Document
-record User(@MongoId String id, String name, String username, String email, Address address) {
+record User(@MongoId String id, String name, String username, String email, Address address, @ReadOnlyProperty UserType type) {
 	record Address(String street, String suite, String city, String zipcode, Geo geo) {
 		record Geo(String lat, String lng) {}
 	}
@@ -108,4 +121,7 @@ interface JsonPlaceholderClient {
 
 	@GetExchange("/users/{id}")
 	Mono<User> getUserById(@PathVariable String id);
+
+	@GetExchange("/users")
+	Mono<User> getUserByUsername(@RequestParam String username);
 }
